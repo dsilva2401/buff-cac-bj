07b0c6756761f33d901687e9658c166f8add0c08
Arqam Saeed <arqam105@outlook.com>
2021-12-16 10:47:57 +0500

    Merge passwordless

--- src/pages/MagicLink/MagicLink.tsx
+++ src/pages/MagicLink/MagicLink.tsx
@@ -1,46 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import qs from 'query-string';
-import { useLocation } from 'react-router-dom';
-import { getAuth, isSignInWithEmailLink, signInWithEmailLink, User } from 'firebase/auth';
-import LoadingIndicator from 'components/LoadingIndicator';
-import useRedirectLoggedInUser from 'hooks/useRedirectLoggedInUser';
-import { useGlobal } from 'context/global/GlobalContext';
+import qs from "query-string";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useGlobal } from "context/global/GlobalContext";
+import LoadingIndicator from "components/LoadingIndicator";
+import useRedirectLoggedInUser from "hooks/useRedirectLoggedInUser";
+import {
+  User,
+  getAuth,
+  signInWithEmailLink,
+  isSignInWithEmailLink,
+} from "firebase/auth";
 
 const MagicLink = () => {
-    const [user, setUser] = useState<User | undefined>();
-    const [token, setToken] = useState<string | undefined>();
-    const { signInRedirect }  = useGlobal();
+  const [user, setUser] = useState<User | undefined>();
+  const [token, setToken] = useState<string | undefined>();
 
-    const location = useLocation();
-    const { email } = qs.parse(location.search);
+  const auth = getAuth();
+  const location = useLocation();
+  const { email } = qs.parse(location.search);
+  const { signInRedirect } = useGlobal();
 
-    const auth = getAuth();
+  useRedirectLoggedInUser(token, user, signInRedirect);
+  useEffect(() => {
+    async function fetchData() {
+      const token = await user?.getIdToken();
+      setToken(token);
+    }
+    if (user) fetchData();
+  }, [user]);
 
-    useRedirectLoggedInUser(token, user, signInRedirect);
+  useEffect(() => {
+    if (isSignInWithEmailLink(auth, window.location.href)) {
+      signInWithEmailLink(auth, email as string, window.location.href)
+        .then((userCredentials) => {
+          setUser(userCredentials.user);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [auth, email]);
 
-    useEffect(() => {
-        async function fetchData() {
-          const token = await user?.getIdToken();
-          setToken(token);
-        }
-        if (user) fetchData();
-      }, [user]);
-
-    useEffect(() => {
-        if (isSignInWithEmailLink(auth, window.location.href)) {
-            signInWithEmailLink(auth, email as string, window.location.href)
-                .then((userCredentials) => {
-                    setUser(userCredentials.user);
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-        }
-    }, [email]);
-
-    return (
-        <LoadingIndicator />
-    );
+  return <LoadingIndicator />;
 };
 
 export default MagicLink;
