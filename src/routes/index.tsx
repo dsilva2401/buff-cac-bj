5282c4c2bd6cc83d251a5a7008d8f4015053045a
Sushrutha Ravish <sushrutha@users.noreply.github.com>
2022-01-04 21:28:15 -0500

    changed routes based on discusison with Ali (#23)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,28 @@
-import { Route, Switch } from "react-router-dom";
-import Login from "pages/Login";
-import SignUp from "pages/SignUp";
-import ForgotPassword from "pages/ForgotPassword";
-import ProductDetails from "pages/ProductDetails";
-import Collection from "pages/Collection";
-import Profile from "pages/Profile/Profile";
-import ResetPassword from "pages/ResetPassword";
-import PersonalDetails from "pages/PersonalDetails"
-import MagicLink from "pages/MagicLink";
-import FourZeroFour from "pages/404";
+import FourZeroFour from 'pages/404';
+import Collection from 'pages/Collection';
+import ForgotPassword from 'pages/ForgotPassword';
+import Login from 'pages/Login';
+import MagicLink from 'pages/MagicLink';
+import PersonalDetails from 'pages/PersonalDetails';
+import ProductDetails from 'pages/ProductDetails';
+import Profile from 'pages/Profile/Profile';
+import ResetPassword from 'pages/ResetPassword';
+import SignUp from 'pages/SignUp';
+import { Route, Switch } from 'react-router-dom';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path={"/"} component={Login} />
-      <Route exact path={"/signup"} component={SignUp} />
-      <Route exact path={"/forgot-password"} component={ForgotPassword} />
-      <Route exact path={"/collection"} component={Collection} />
-      <Route exact path={"/profile"} component={Profile} />
-      <Route exact path={"/reset-password"} component={ResetPassword} />
-      <Route exact path={"/product/:id"} component={ProductDetails} />
-      <Route exact path={"/magic-link"} component={MagicLink} />
-      <Route exact path={"/personal-details"} component={PersonalDetails} />
-      <Route exact path={"/404"} component={FourZeroFour} />
+      <Route exact path={'/'} component={Login} />
+      <Route exact path={'/app/signup'} component={SignUp} />
+      <Route exact path={'/app/forgot-password'} component={ForgotPassword} />
+      <Route exact path={'/app/collection'} component={Collection} />
+      <Route exact path={'/app/profile'} component={Profile} />
+      <Route exact path={'/app/reset-password'} component={ResetPassword} />
+      <Route exact path={'/c/:id'} component={ProductDetails} />
+      <Route exact path={'/app/magic-link'} component={MagicLink} />
+      <Route exact path={'/app/personal-details'} component={PersonalDetails} />
+      <Route component={FourZeroFour} />
     </Switch>
   );
 }
