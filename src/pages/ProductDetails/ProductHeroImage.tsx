af0ef320d43d76b62a09281664ce5a34dd547287
Diego <dsilva2401@gmail.com>
2022-06-12 20:03:11 -0500

    Images optimization support added (#194)
    Co-authored-by: Diego Silva <diego@brij.it>

--- src/pages/ProductDetails/ProductHeroImage.tsx
+++ src/pages/ProductDetails/ProductHeroImage.tsx
@@ -1,47 +1,81 @@
 import Image from 'components/Image';
 import Wrapper from 'components/Wrapper';
 import { useGlobal } from 'context/global/GlobalContext';
+import { Fragment, useState } from 'react';
 import { Animated } from 'react-animated-css';
 import ProgressiveImage from 'react-progressive-image';
 
 const ProductHeroImage = () => {
   const { productDetails, appZoom, collapsedDrawerHeight } = useGlobal();
-
-  if (!productDetails) {
-    return null;
+  const backgroundColor = productDetails?.brand?.customBgColor || 'white';
+  const name = productDetails?.brand?.name;
+  const imageSrc = productDetails?.product?.image || '';
+  let optimizedImageSrc = imageSrc;
+  let thumbnailSrc = '';
+  if (!imageSrc.match(/shopify/gi)) {
+    const fileId = imageSrc
+      .split('?')
+      .shift()
+      ?.split('/')
+      .pop()
+      ?.split('.')
+      .shift();
+    thumbnailSrc = `${
+      process.env.REACT_APP_API_URL || ''
+    }/optimized-images/${fileId}/thumbnail.webp`;
+    optimizedImageSrc = `${
+      process.env.REACT_APP_API_URL || ''
+    }/optimized-images/${fileId}/1000x1000.webp`;
   }
-
-  const backgroundColor = productDetails.brand?.customBgColor || 'white';
-  const { name } = productDetails.brand;
-  const imageSrc = productDetails.product?.image || '';
+  const [renderOptimizedImage, setRenderOptimizedImage] = useState(true);
 
   return (
-    <ProgressiveImage src={imageSrc} placeholder=''>
-      {(src: string, loading: boolean) => {
-        return loading ? (
-          <Wrapper width='100%' height='100%' background={backgroundColor} />
-        ) : (
-          <Wrapper
-            width='100%'
-            height={`${window.innerHeight / appZoom}px`}
-            padding={`0 0 ${collapsedDrawerHeight + 40}px 0`}
-            background={backgroundColor}
-          >
-            <Animated isVisible animationIn='fadeIn' animationOut='fadeIn'>
-              <Image
-                src={src}
-                alt={name}
-                position='relative'
+    <Fragment>
+      {!!productDetails && (
+        <ProgressiveImage
+          onError={() => {
+            setRenderOptimizedImage(false);
+          }}
+          src={renderOptimizedImage ? optimizedImageSrc : imageSrc}
+          placeholder={thumbnailSrc}
+        >
+          {(src: string, loading: boolean) => {
+            return !!loading ? (
+              <Wrapper width='100%' height='100%' background={backgroundColor}>
+                <div
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    filter: 'blur(7px)',
+                    background: `url(${src}) no-repeat center center`,
+                    backgroundSize: 'cover',
+                  }}
+                ></div>
+              </Wrapper>
+            ) : (
+              <Wrapper
                 width='100%'
-                margin='auto'
-                objectFit='cover'
-                style={{ minHeight: '100%' }}
-              />
-            </Animated>
-          </Wrapper>
-        );
-      }}
-    </ProgressiveImage>
+                height={`${window.innerHeight / appZoom}px`}
+                padding={`0 0 ${collapsedDrawerHeight + 40}px 0`}
+                background={backgroundColor}
+              >
+                <Animated isVisible animationIn='fadeIn' animationOut='fadeIn'>
+                  <Image
+                    src={src}
+                    alt={name}
+                    position='relative'
+                    width='100%'
+                    margin='auto'
+                    objectFit='cover'
+                    style={{ minHeight: '100%' }}
+                  />
+                </Animated>
+              </Wrapper>
+            );
+          }}
+        </ProgressiveImage>
+      )}
+    </Fragment>
   );
 };
 
