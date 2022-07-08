729d511fccc8b775b7a64217f850a773e75dec8b
Arqam Saeed <arqam105@outlook.com>
2022-07-08 06:02:10 +0500

    Disable Screen Rotation (#241)
    * Landing page hotfix
    
    * Disable view on landscape mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { useLayoutEffect, useEffect } from 'react';
 import { isBrowser } from 'react-device-detect';
 import { BrowserRouter } from 'react-router-dom';
 import { useGlobal } from './context/global/GlobalContext';
+import { getAuth, getRedirectResult } from 'firebase/auth';
+import { MAGIC_ACTION } from 'context/global/GlobalProvider';
 import { LastLocationProvider } from 'react-router-last-location';
 import AppContainer from 'components/AppContainer/AppContainer';
 import AppFrame from 'components/AppFrame/AppFrame';
 import SideMenu from 'components/SideMenu/SideMenu';
 import GlobalStyle from 'styles/global';
 import Toast from 'components/Toast';
+import Text from 'components/Text';
 import Routes from './routes';
-import { getAuth, getRedirectResult } from 'firebase/auth';
-import { MAGIC_ACTION } from 'context/global/GlobalProvider';
 
 export default function App() {
   const {
@@ -65,18 +66,28 @@ export default function App() {
   }
 
   const browserRouter = (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <LastLocationProvider>
-        <SideMenu />
-        <Routes />
-      </LastLocationProvider>
-    </BrowserRouter>
+    <div id='portrait'>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <LastLocationProvider>
+          <SideMenu />
+          <Routes />
+        </LastLocationProvider>
+      </BrowserRouter>
+    </div>
   );
 
   return (
     <AppContainer isBrowser={isBrowser} style={{ zoom: appZoom }}>
       <GlobalStyle />
       {isBrowser ? <AppFrame>{browserRouter}</AppFrame> : browserRouter}
+      <Text
+        id='landscape'
+        fontSize='18px'
+        textAlign='center'
+        wrapperWidth='max-content'
+      >
+        <span>Please rotate device for Brij experience</span>
+      </Text>
       <Toast />
     </AppContainer>
   );
--- src/components/AppContainer/AppContainer.ts
+++ src/components/AppContainer/AppContainer.ts
@@ -11,6 +11,22 @@ const AppContainer = styled.div<AppContainerProps>`
   align-items: center;
   justify-content: center;
   background-color: ${(props) => (props.isBrowser ? '#F5F5F5' : '#FFFFFF')};
+  @media screen and (orientation: landscape) {
+    #landscape {
+      display: flex;
+    }
+    #portrait {
+      display: none;
+    }
+  }
+  @media screen and (orientation: portrait) {
+    #landscape {
+      display: none;
+    }
+    #portrait {
+      display: flex;
+    }
+  }
 `;
 
 export default AppContainer;
